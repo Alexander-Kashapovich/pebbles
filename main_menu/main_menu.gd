8f5b1ce996ae6c56f9403832b269ba96ba6ff0f9
Alexander-Kashapovich <167694496+Alexander-Kashapovich@users.noreply.github.com>
2026-02-12 12:39:26 +0300

    add_buttons_to_menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,11 +1,43 @@
 extends Control
 class_name MainMenu
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_quit()
 func _ready() -> void:
-	$Go.pressed.connect(_go)
-	$Exit.pressed.connect(get_tree().quit)
+	var buts = $"1/Buts/1/Buts".get_children()
+	buts[0].pressed.connect(_continue)
+	buts[1].pressed.connect(_freeplay)
+	buts[2].pressed.connect(_settings)
+	buts[3].pressed.connect(_quit)
 	
-func _go() -> void:
+	$"1/Settings/1/Buts/SFX".value_changed.connect(set_sfx)
+	$"1/Settings/1/Buts/MUSIC".value_changed.connect(set_music)
+	$"1/Settings/1/Buts/Return".pressed.connect(_return_to_menu)
+
+func _continue() -> void:
 	var world = load("res://world/world.tscn").instantiate()
 	add_sibling(world)
 	queue_free()
+
+func _freeplay() -> void:
+	pass
+
+func set_sfx(val:float) -> void:
+	AudioServer.set_bus_volume_linear(2,val)
+	if not $SFX.playing:
+		$SFX.play()
+
+func set_music(val:float) -> void:
+	AudioServer.set_bus_volume_linear(1,val)
+
+func _settings() -> void:
+	$"1/Buts".visible = 0
+	$"1/Settings".visible = 1
+
+func _return_to_menu() -> void:
+	$"1/Buts".visible = 1
+	$"1/Settings".visible = 0
+
+func _quit() -> void:
+	get_tree().quit()
